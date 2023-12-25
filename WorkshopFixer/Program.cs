589c43d9fd465ee8ed6a114da9e395ff2cff54a3
BoyanDimov20 <44165461+BoyanDimov20@users.noreply.github.com>
2023-12-25 19:26:12 +0200

    New Fix

--- WorkshopFixer/Program.cs
+++ WorkshopFixer/Program.cs
@@ -1,84 +1,21 @@
-﻿using Newtonsoft.Json.Linq;
-using System.Globalization;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using Newtonsoft.Json.Linq;
 
-var defaultName = "WorkshopUpload.extracted.json";
+var file = File.ReadAllBytes("WorkshopUpload");
 
-Console.WriteLine("Enter for Default: (WorkshopUpload.extracted.json)");
-Console.Write("Map Name: ");
-var mapName = Console.ReadLine();
-
-if(string.IsNullOrWhiteSpace(mapName))
+MemoryStream ms = new MemoryStream(file);
+using (BsonReader reader = new BsonReader(ms))
 {
-    mapName = defaultName;
+    JsonSerializer serializer = new JsonSerializer();
+
+    var result = (JObject)serializer.Deserialize(reader);
+
+
+    string json = result.ToString(Newtonsoft.Json.Formatting.Indented);
+    
+    File.WriteAllText("WorkshopUpload.json", json);
 }
 
-var text = File.ReadAllText(mapName);
-
-
-var json = JObject.Parse(text);
-ReplaceJson(json);
-
-
-var fileResult = Path.GetFileNameWithoutExtension(mapName);
-File.WriteAllText($"{fileResult}_fixed.json", json.ToString(Newtonsoft.Json.Formatting.Indented));
-
-Console.WriteLine("Fixed!");
-Console.WriteLine("Done By SorionN");
-Thread.Sleep(2000);
-
-
-
-void ReplaceJson(JToken token)
-{
-    if (token.Type == JTokenType.Property)
-    {
-        var prop = (JProperty)token;
-        if (prop.Name == "$numberInt")
-        {
-            if (token.Parent.Parent.Type == JTokenType.Array)
-            {
-                token.Parent.Replace(int.Parse(prop.Value.ToString()));
-            }
-            else if (token.Parent.Parent.Type == JTokenType.Property)
-            {
-                var curr = (JProperty)token;
-                var parent = (JProperty)token.Parent.Parent;
-                token.Parent.Parent.Replace(new JProperty(parent.Name, int.Parse(curr.Value.ToString())));
-            }
-        }
-        else if (prop.Name == "$numberDouble")
-        {
-            if (token.Parent.Parent.Type == JTokenType.Array)
-            {
-                token.Parent.Replace(Convert.ToDouble(prop.Value.ToString(), CultureInfo.InvariantCulture));
-            }
-            else if (token.Parent.Parent.Type == JTokenType.Property)
-            {
-                var curr = (JProperty)token;
-                var parent = (JProperty)token.Parent.Parent;
-                token.Parent.Parent.Replace(new JProperty(parent.Name, Convert.ToDouble(curr.Value.ToString(), CultureInfo.InvariantCulture)));
-            }
-        }
-        else if (prop.Name == "$numberLong")
-        {
-            if (token.Parent.Parent.Type == JTokenType.Array)
-            {
-                token.Parent.Replace(long.Parse(prop.Value.ToString()));
-            }
-            else if (token.Parent.Parent.Type == JTokenType.Property)
-            {
-                var curr = (JProperty)token;
-                var parent = (JProperty)token.Parent.Parent;
-                token.Parent.Parent.Replace(new JProperty(parent.Name, long.Parse(curr.Value.ToString())));
-            }
-        }
-    }
-    if (token.Next != null)
-    {
-        ReplaceJson(token.Next);
-    }
-    if (token.Children().Count() > 0)
-    {
-        ReplaceJson(token.First());
-    }
-}
+Console.WriteLine("Done by SorionN <3");
+Thread.Sleep(1000);
